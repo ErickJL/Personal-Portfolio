1d3ed26b9144e84709843d1c0a5a8b86f3c3107d
Erick Lalihatu <erick.lalihatu@jatis.com>
2024-12-09 01:44:04 +0700

    update base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/personal-portfolio/",
+  base: "/Personal-Portfolio/",
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
